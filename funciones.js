0bdde1c17225a1d0d78173bf36493e6fd574a7bb
Javier Aguirre <javieraguirreco10@gmail.com>
2022-08-24 16:00:09 -0400

    Se siguen recomendaciones del tutor y se modifica el archivo .js

--- funciones.js
+++ funciones.js
@@ -1,4 +1,4 @@
-let eligePokemon = prompt('¡Bienvenido a la región de Johto! Para dar inicio a tu aventura Pokémon, elige a tu compañero pokémon de la región.')
+let eligePokemon = prompt('¡Bienvenido a la región de Johto! Para dar inicio a tu aventura Pokémon, elige a tu compañero pokémon de la región: \nChikorita \nCyndaquil \nTotodile')
 const primerOponente = 'Houndour'
 const segundoOponente = 'Rowlet'
 const tercerOponente = 'Fennekin'
@@ -6,13 +6,17 @@ const tercerOponente = 'Fennekin'
 while((eligePokemon !== 'chikorita') && (eligePokemon !== 'cyndaquil') && (eligePokemon !== 'totodile')){
     alert('Elige un Pokémon correcto.');
     eligePokemon = prompt('¡Bienvenido a la región de Johto! Para dar inicio a tu aventura Pokémon, elige a tu compañero pokémon de la región.')
-} if(eligePokemon === 'chikorita'){
-    alert('¡Felicidades! El Profesor Elm te ha dado un ' +eligePokemon+ ' con 100 PS (puntos de salud).');
-    alert('Tu enemigo será ' +primerOponente)
-} else if(eligePokemon === 'cyndaquil'){
-    alert('¡Felicidades! El Profesor Elm te ha dado un ' +eligePokemon+ ' con 100 PS (puntos de salud).');
-    alert('Tu enemigo será '+segundoOponente)
-} else if(eligePokemon === 'totodile'){
-    alert('¡Felicidades! El Profesor Elm te ha dado un ' +eligePokemon+ ' con 100 PS (puntos de salud).');
-    alert('Tu enemigo será '+tercerOponente)
-} alert('¡Prepárate para la aventura!')
+
+    if(eligePokemon === 'chikorita'){
+        alert('¡Felicidades! El Profesor Elm te ha dado un ' +eligePokemon+ ' con 100 PS (puntos de salud).');
+        alert('Tu enemigo será ' +primerOponente)
+    } else if(eligePokemon === 'cyndaquil'){
+        alert('¡Felicidades! El Profesor Elm te ha dado un ' +eligePokemon+ ' con 100 PS (puntos de salud).');
+        alert('Tu enemigo será '+segundoOponente)
+    } else if(eligePokemon === 'totodile'){
+        alert('¡Felicidades! El Profesor Elm te ha dado un ' +eligePokemon+ ' con 100 PS (puntos de salud).');
+        alert('Tu enemigo será '+tercerOponente)
+    }
+} 
+
+alert('¡Prepárate para la aventura!')
